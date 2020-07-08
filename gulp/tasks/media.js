0cf1643373d9977272dce741b22b89789026a03d
j-oshin <jtoshin@msn.com>
2020-07-08 18:10:46 +0100

    compress image

---
+++ gulp/tasks/media.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const { src, dest, series } = require('gulp');
+
+const image = require('gulp-image');
+
+function compressImage() {
+    return src('./app/img/*')
+        .pipe(image())
+        .pipe(dest('./dist/img/'));
+}
+
+exports.task = compressImage;
--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,9 @@ const { task, parallel } = require('gulp');
 
 const css = require('./gulp/tasks/css');
 const js = require('./gulp/tasks/script');
+const img = require('./gulp/tasks/media');
 
-const buildCssJs = parallel(css.task, js.task);
+const buildCssJs = parallel(css.task, js.task, img.task);
 
 task('buildCssJs', buildCssJs);
 
