bc7f314105d43c1c478d2d1bcb951b2db52efeaf
jayosh65 <jtoshin@gmail.com>
2020-07-11 09:30:21 +0100

    add autoprefixer

--- gulp/tasks/css.js
+++ gulp/tasks/css.js
@@ -10,7 +10,9 @@ const cleanCSS = require('gulp-clean-css');
 const rename = require("gulp-rename");
 const concat = require('gulp-concat');
 const sourcemaps = require('gulp-sourcemaps');
+const autoprefixer = require('gulp-autoprefixer');
 
+// Path to exclude using globbng patterns
 let paths = {
   libs: [css.path.css.src + '**/*.scss', '!' + css.path.css.src + 'components/**/*.scss' ]
 }
@@ -33,9 +35,10 @@ function compileToCss() {
 function minifyCss() {
   return src(css.path.css.tmp + '*.css')
     .pipe(sourcemaps.init())
+    .pipe(autoprefixer('last 2 versions'))
     .pipe(cleanCSS({compatibility: 'ie8'}))
     .pipe(rename({extname: ".min.css"}))
-    .pipe(sourcemaps.write())
+    .pipe(sourcemaps.write('./'))
     .pipe(dest(css.path.css.dist));
 }
 
