754fea1c59d0faa241736f0972c5b9d46e577fd5
j-oshin <jtoshin@msn.com>
2020-07-12 17:43:25 +0100

    watch tasks

--- gulp/tasks/cleanFolders.js
+++ gulp/tasks/cleanFolders.js
@@ -1,19 +1,21 @@
 'use strict';
 
-const { src, dest, parallel } = require('gulp');
+const { src } = require('gulp');
 
 const clean = require('gulp-clean');
 
 function cleanCss() {
-  return src('./dist/styles').pipe(clean())
+  return src(['./tmp/styles', './dist/styles'], {read: false, allowEmpty: true}).pipe(clean())
 }
 
 function cleanJs() {
-  return src('./dist/js').pipe(clean())
+  return src('./dist/js', {read: false, allowEmpty: true}).pipe(clean())
 }
 
 function cleanImage() {
-  return src('./dist/img').pipe(clean())
+  return src('./dist/img', {read: false, allowEmpty: true}).pipe(clean())
 }
 
-exports.task = parallel(cleanCss, cleanJs, cleanImage);
+exports.cssTask = cleanCss;
+exports.jsTask = cleanJs;
+exports.imgTask = cleanImage;
--- gulpfile.js
+++ gulpfile.js
@@ -5,73 +5,98 @@ const css = require('./gulp/tasks/css');
 const js = require('./gulp/tasks/script');
 const img = require('./gulp/tasks/media');
 
+
+task('cleanCss', cleanFolders.cssTask);
+task('cleanJs', cleanFolders.jsTask);
+task('cleanImg', cleanFolders.imgTask);
+task('cleanAll', parallel(cleanFolders.cssTask, cleanFolders.jsTask, cleanFolders.imgTask));
+
 task('buildCss', css.task);
 task('buildJs', js.task);
 task('compressImg', img.task);
-// const buildCssJs = parallel(css.task, js.task, img.task);
 
-// const build = series(cleanFolders.task, buildCssJs)
+function watchTask(done) {
+    const watcher = watch(['app/js/**/*.js', 'app/scss/**/*.scss', 'app/img/"**/*.+(png|jpg|gif|svg)'], series('cleanAll', parallel('buildJs', 'buildCss', 'compressImg')));
 
-// task('buildCssJs', build);
+    watcher.on('change', async function(path, stats) {
+      console.log(`File ${path} was changed`);
+    });
+
+    watcher.on('add', async function(path, stats) {
+      console.log(`File ${path} was added`);
+    });
+
+    watcher.on('unlink', async function(path, stats) {
+      console.log(`File ${path} was removed`);
+    });
+
+    done();
+}
+
+exports.watchAll = watchTask;
+
+function watchCssTask(done) {
+    const watcher = watch(['app/scss/**/*.scss'], series('cleanCss', 'buildCss'));
+
+    watcher.on('change', async function(path, stats) {
+      console.log(`File ${path} was changed`);
+    });
+
+    watcher.on('add', async function(path, stats) {
+      console.log(`File ${path} was added`);
+    });
+
+    watcher.on('unlink', async function(path, stats) {
+      console.log(`File ${path} was removed`);
+    });
+
+    done();
+}
+
+exports.watchCss = watchCssTask;
+
+function watchJsTask(done) {
+    const watcher = watch('app/js/**/*.js', series('cleanJs', 'buildJs'));
+
+    watcher.on('change', async function(path, stats) {
+      console.log(`File ${path} was changed`);
+    });
+
+    watcher.on('add', async function(path, stats) {
+      console.log(`File ${path} was added`);
+    });
+
+    watcher.on('unlink', async function(path, stats) {
+      console.log(`File ${path} was removed`);
+    });
+
+    done();
+}
+
+exports.watchJs = watchJsTask;
+
+function watchImgTask(done) {
+    const watcher = watch(['app/img/**/*.+(png|jpg|gif|svg)'], series('cleanImg', 'compressImg'));
+
+    watcher.on('change', async function(path, stats) {
+      console.log(`File ${path} was changed`);
+    });
+
+    watcher.on('add', async function(path, stats) {
+      console.log(`File ${path} was added`);
+    });
+
+    watcher.on('unlink', async function(path, stats) {
+      console.log(`File ${path} was removed`);
+    });
+
+    done();
+}
+
+exports.watchImg = watchImgTask;
 
 
 
-// function watchTask(done){
-//     // watch(['app/js/*.js'], function(cb) {
-//     // // body omitted
-//     // console.log("HTTP Server Started");
-//     // cb();
-//     // });
-
-
-//     const watcher = watch(['app/js/*.js']);
-
-//     watcher.on('change', function(path, stats) {
-//       console.log(`File ${path} was changed`);
-//     });
-    
-//     watcher.on('add', function(path, stats) {
-//       console.log(`File ${path} was added`);
-//     });
-    
-//     watcher.on('unlink', function(path, stats) {
-//       console.log(`File ${path} was removed`);
-//     });
-
-//     console.log("HTTP Server Started");
-    
-//     // watcher.close();
-
-//     done();
-// }
-
-// exports.default = watchTask;
-
-// const { watch, task } = require('gulp');
-
-// const tests = function test(done) {
-//     const watcher = watch(['app/js/*.js']);
-
-//     watcher.on('change', function(path, stats) {
-//       console.log(`File ${path} was changed`);
-//     });
-    
-//     watcher.on('add', function(path, stats) {
-//       console.log(`File ${path} was added`);
-//     });
-    
-//     watcher.on('unlink', function(path, stats) {
-//       console.log(`File ${path} was removed`);
-//     });
-
-//     console.log("HTTP Server Started");
-    
-//     watcher.close();
-
-//     done();
-// };
-
-// task('watch', tests);
 
 // gulp.task('watch', gulp.series('some-task-name', function () {
 //     browserSync.init({
@@ -81,10 +106,10 @@ task('compressImg', img.task);
 //     });
 
 //     var watcher = gulp.watch([
-//                     './src/views/*.html', 
-//                     './src/index.html', 
-//                     './src/assets/css/*.css', 
-//                     './src/**/*.js'],
+//                      'app/js/**/*.js', 
+//                         'app/scss/**/*.scss', 
+//                         'app/img/"**/*.+(png|jpg|gif|svg)'
+//                     ],
 //                     gulp.series('some-task-name'));
 //     watcher.on('change', async function (path, stats) {
 //         console.log('you changed the code');
